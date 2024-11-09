4756f4624d0db89869357a335dd23999b69374ce
ganyu <ganyu21@qq.com>
2024-11-09 23:24:34 +0800

    fix third point

--- chan.py
+++ chan.py
@@ -9,11 +9,12 @@ from datetime import datetime, timedelta
 缠中说禅分析
 '''
 
+
 class ChanLun:
 
     def __init__(self, klines):
-        self.klines_orig_df = klines # 原始K线, dataframe格式
-        self.klines_orig = klines_h.to_dict(orient='records')  # 原始K线, list格式
+        self.klines_orig_df = klines  # 原始K线, dataframe格式
+        self.klines_orig = klines.to_dict(orient='records')  # 原始K线, list格式
         self.klines_merge = []  # 合并后的K线
         self.fxs = []  # 分型
         self.pens = []  # 笔列表
@@ -100,7 +101,7 @@ class ChanLun:
                              yaxis_opts=opts.AxisOpts(is_scale=True,
                                                       splitarea_opts=opts.SplitAreaOpts(is_show=True, areastyle_opts=opts.AreaStyleOpts(opacity=1)),
                                                       ),
-                             datazoom_opts=[opts.DataZoomOpts()],
+                             datazoom_opts=[opts.DataZoomOpts(range_start=0, range_end=100)],
                              title_opts=opts.TitleOpts(title=self.klines_merge[0]['code']),
                              )
             .set_series_opts(
@@ -159,9 +160,10 @@ class ChanLun:
 
     '''
     计算中枢
+    {'start_datetime': Timestamp('2024-02-23 00:00:00'), 'low_price': 9.35, 'high_price': 9.79, 'end_datetime': Timestamp('2024-04-12 00:00:00'), 'signal_price': 9.85, 'signal_datetime': Timestamp('2024-05-15 00:00:00'), 'mark': 'up', 'layer': 1}
     '''
 
-    def kline_hub(self, bold=False):
+    def kline_hub(self):
         pen_len = len(self.pens)
         if pen_len < 7:
             print("pen is less then 7，cant calculate hub")
@@ -278,7 +280,7 @@ class ChanLun:
                     self.is_in_hub = False
 
     '''
-    笔划分，笔包括开始点分型、类型（up或down）、分型中间点最值价格
+    笔划分，笔包括 开始分型（类型、最低和最高、开始分型的三个点，中间点的时间）、笔类型（up或down）、开始分型中间点的最值价格
     {'fx': {'mark': 'h', 'low': 16.54, 'high': 18.1,
     'k1': {'datetime': '2020-10-16', 'code': 'sz.000001', 'open': 16.56, 'high': 17.37, 'low': 16.54, 'close': 17.1, 'volume': 209561419, 'amount': 3589229558.57, 'adjustflag': 2}, 
     'k2': {'datetime': '2020-10-19', 'code': 'sz.000001', 'open': 17.3, 'high': 18.1, 'low': 17.3, 'close': 17.48, 'volume': 201610552, 'amount': 3571336006.25, 'adjustflag': 2}, 
@@ -381,8 +383,8 @@ class ChanLun:
                 fx['k2'] = second_k
                 fx['k3'] = third_k
 
-                kdj=self.get_kdj(second_k['datetime'])
-                fx['K']=kdj['K'].iloc[-1]
+                kdj = self.get_kdj(second_k['datetime'])
+                fx['K'] = kdj['K'].iloc[-1]
                 fx['D'] = kdj['D'].iloc[-1]
                 fx['J'] = kdj['J'].iloc[-1]
                 self.fxs.append(fx)
@@ -524,18 +526,18 @@ class ChanLun:
     '''
     得到最近已经形成的指定层数的中枢
     hub_direct：中枢趋势方向
-    hub_layer：指定中枢层数, 0代表忽略，1代表第一层中枢，2代表第二层中枢，以此类推
+    hub_layer：指定中枢层数,数组形式[1,2,3], 1代表第一层中枢，2代表第二层中枢，以此类推
     '''
 
     # direct is up or down
-    def get_lastest_hub(self, hub_direct='up', hub_layer=0):
+    def get_lastest_hub(self, hub_direct='up', hub_layers=None):
+        if hub_layers is None:
+            hub_layers = [1, 2, 3]
         hub_count = len(self.hubs)
         if hub_count > 0:
             hub_tail = self.hubs[-1]
             if hub_tail['mark'] == hub_direct:
-                if hub_layer == 0:
-                    return hub_tail
-                elif hub_tail['layer'] == hub_layer:
+                if hub_tail['layer'] in hub_layers:
                     return hub_tail
         return None
 
@@ -543,12 +545,12 @@ class ChanLun:
     是否是第三买入或卖出点
     '''
 
-    def is_third_point(self, hub_direct='up', hub_layer=0):
-        hub_tail = self.get_lastest_hub(hub_direct, hub_layer)
-        if not hub_tail:
-            return True
+    def get_third_point_datetime(self, hub_direct='up', hub_layers=None):
+        hub_tail = self.get_lastest_hub(hub_direct, hub_layers)
+        if hub_tail:
+            return hub_tail['signal_datetime']
         else:
-            return False
+            return None
 
 
 if __name__ == "__main__":
@@ -559,28 +561,29 @@ if __name__ == "__main__":
     'low': 16.54, 'close': 17.1, 'volume': 209561419,}
     '''
     symbol = "000001"
-    start_datetime = datetime(1999, 8, 31, 11, 15, 0)
-    end_datetime = datetime(2099, 9, 5, 11, 15, 0)
-    # start_datetime = datetime(2009, 7, 8, 0, 0, 0)
-    # end_datetime = datetime(2012, 7, 20, 0, 0, 0)
+    # start_datetime = datetime(1999, 8, 31, 11, 15, 0)
+    # end_datetime = datetime(2099, 9, 5, 11, 15, 0)
+    start_datetime = datetime(2023, 11, 8, 0, 0, 0)
+    end_datetime = datetime(2024, 5, 27, 0, 0, 0)
 
+    # symbol = "v2305"
 
-    klines_h = pd.read_csv("./" + symbol + "_1d.csv")
-    klines_h['datetime'] = pd.to_datetime(klines_h['datetime'])
+    klines = pd.read_csv('./' + "stock/" + symbol + "_1d.csv")
+    klines['datetime'] = pd.to_datetime(klines['datetime'])
     # 使用布尔索引来筛选出'datetime'列小于今天的所有行
-    klines_h = klines_h[(klines_h['datetime'] <= end_datetime) & (klines_h['datetime'] >= start_datetime)]
-    klines_h = klines_h.sort_values('datetime')
-    klines_h['code'] = symbol
-    klines_h.set_index('datetime', inplace=True)
-    klines_h = klines_h.reset_index()
+    klines = klines[(klines['datetime'] <= end_datetime) & (klines['datetime'] >= start_datetime)]
+    klines = klines.sort_values('datetime')
+    klines['code'] = symbol
+    klines.set_index('datetime', inplace=True)
+    klines = klines.reset_index()
 
-    if len(klines_h) > 0:
-        chan_h = ChanLun(klines_h)
+    if len(klines) > 0:
+        chan = ChanLun(klines)
         # 得到最近已经形成的指定层数的中枢
-        hub_tail = chan_h.get_lastest_hub(hub_direct='up', hub_layer=0)
+        hub_tail = chan.get_lastest_hub(hub_direct='up', hub_layers=[1,2,3])
         print('hub_tail', hub_tail)
-        print('is_in_hub', chan_h.is_in_hub)
+        print('is_in_hub', chan.is_in_hub)
         # 是否第三买入或卖出点
-        third_point = chan_h.is_third_point(hub_direct='up', hub_layer=0)
+        third_point = chan.get_third_point_datetime(hub_direct='up', hub_layers=[1,2,3])
         print('third_point', third_point)
-        chan_h.draw(symbol)
+        chan.draw(symbol)
